751d91439929c8880fd56f7def2288eef6c94711
Asjad Mahmood <38811693+AsjadMahmood@users.noreply.github.com>
2020-07-18 16:34:15 +0500

    Update unsplash.js
    new commit

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID o9iRiT7STKKFhI6kQgUKqOupvkFXJaB5KbOKe_CTWDA'
+      Authorization: 'your authorization header from unspalsh api'
     }
 })
